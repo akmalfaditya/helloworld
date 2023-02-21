8a8bdddf4c2016da7a259707f39366b44aeb130c
akmal <akmalfaditya@gmail.com>
2023-02-21 12:41:03 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello Worl',
+            style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic),
+          ),
+        ),
+      )
+    );
+  }
+}
+
+
